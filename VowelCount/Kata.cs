fc3953944355d778f89b3832016d51e468b60134
Alejandrorodram <ale1.zul@gmail.com>
2022-03-06 11:19:23 +0000

    first commit

---
+++ VowelCount/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace VowelCount
+{
+    internal class Kata
+    {
+        public static int GetVowelCount1(string str)
+        {
+            return str.Count(i => "aeiou".Contains(i));
+        }
+
+        public static int GetVowelCount2(string str)
+        {
+            return str.ToLower().Count(c => "aeiou".IndexOf(c) != -1);
+        }
+
+        public static int GetVowelCount3(string str)
+        {
+            return (Regex.Matches(str, @"[aeiouAEIOU]")).Count;
+        }
+    }
+}
---
+++ VowelCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetVowelCount1("Contar las vocales de este texto"));
+            Console.WriteLine(Kata.GetVowelCount2("Contar las vocales de este texto"));
+            Console.WriteLine(Kata.GetVowelCount3("Contar las vocales de este texto"));
+
+        }
+    }
+}
